003afbb237e6f58b3e06b50a6c4659555db7891c
Hanna Svietik <hsvietik@gmail.com>
2023-04-06 20:57:37 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${preview}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", picturePreview);
+
+function picturePreview(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains("gallery__image")) {
+    const instance = basicLightbox.create(`
+  <div class="modal">
+  <img class="gallery__image" src=${evt.target.dataset.source} alt="${evt.target.alt}">
+  </div>
+`);
+    instance.visible() ? instance.close() : instance.show();
+    document.addEventListener("keydown", (evt) => {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}
