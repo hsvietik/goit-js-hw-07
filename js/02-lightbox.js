b48664409a4fbbae1483bbba2043a5cfcbe4e678
Hanna Svietik <hsvietik@gmail.com>
2023-04-07 14:20:06 +0100

    some changes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
+const gallery2 = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
     ({ preview, original, description }) => `<li class="gallery__item">
@@ -10,7 +10,7 @@ const markup = galleryItems
 </li>`
   )
   .join("");
-gallery.insertAdjacentHTML("beforeend", markup);
+gallery2.insertAdjacentHTML("beforeend", markup);
 var lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
